5cb777bfafa6a15b5b6cd0b27334c2c3e8c4c5a1
Daniel Weiler <danielweiler78@gmail.com>
2021-07-05 20:32:29 +0300

    Implement app with heroku and front-end build

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
+app.use(express.static('build'))
+app.use(cors())
 app.use(express.json())
-
 morgan.token('person', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :person'))
-
 
 let persons = [
     { 
@@ -98,7 +99,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
